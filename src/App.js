f9f157a63c30221a0e273ab62f40881e47b3db18
didichulam <140446190+didichulam@users.noreply.github.com>
2024-07-31 21:32:15 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>Dictionary App</h1>
+    </div>
+  );
+}
+
+export default App;
